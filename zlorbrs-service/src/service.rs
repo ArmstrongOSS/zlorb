57ce7565ccbe06aaf7c7d20adb49b519c3cff553
Triston Armstrong <tarmstrong95@proton.me>
2026-05-23 22:51:18 -0600

    feat: implement service coordination structure and logic

---
+++ zlorbrs-service/src/service.rs
@@ -0,0 +1,22 @@
+pub struct ServiceCoordinator {
+    service_config: ServiceConfig,
+    repo_configs: Vec<RepoConfig>,
+    first_run_flag: bool,
+}
+
+impl ServiceCoordinator {
+    fn new(&self) {
+        unimplemented!()
+    }
+
+    fn run_loop(&self) {
+        unimplemented!()
+    }
+    fn wait_for_run(&self) {
+        unimplemented!()
+    }
+
+    fn run_cycle(&self) {
+        unimplemented!()
+    }
+}
