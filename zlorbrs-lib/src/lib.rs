4de389dc1fd71f171d4a4b9e29b23fe8773249a4
Triston Armstrong <tarmstrong95@proton.me>
2026-05-24 20:05:46 -0600

    feat: add file system utility functions

--- zlorbrs-lib/src/lib.rs
+++ zlorbrs-lib/src/lib.rs
@@ -5,6 +5,8 @@ use std::path::PathBuf;
 
 use log::error;
 
+use crate::error::ZlorbError;
+
 pub fn get_home_dir() -> PathBuf {
     let home_dir = match std::env::home_dir() {
         Some(x) => x,
@@ -14,6 +16,26 @@ pub fn get_home_dir() -> PathBuf {
         }
     };
     home_dir
+}
+
+pub fn create_file_with_content(path: PathBuf, content: &String) -> Result<String, ZlorbError> {
+    std::fs::write(path, content).map_err(|e| ZlorbError::Io(e))?;
+    Ok(content.clone())
+}
+
+pub fn read_file_from_filesystem(path: PathBuf) -> Option<String> {
+    let f = std::fs::read_to_string(path);
+    if f.is_err() {
+        return None;
+    }
+    Some(f.unwrap())
+}
+
+pub fn check_file_exist(path: PathBuf) -> bool {
+    match std::fs::exists(path) {
+        Ok(val) => val,
+        Err(_) => false,
+    }
 }
 
 pub mod shared_test_utils {
