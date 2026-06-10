49333c4e5a913cbdf35c99809cc8b870e9557ec1
Joban <2983844+jobansd@users.noreply.github.com>
2026-06-10 00:43:32 -0400

    feat: setup base web ui and wire up web server

--- zlorb-web/src/main.rs
+++ zlorb-web/src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() {
+    let webhook_routes = axum::routing::Router::new().route("/webhook", axum::routing::post(handle_webhook));
+    let frontend_service = axum::routing::get_service(tower_http::services::ServeDir::new("dist"))
+        .handle_error(|_| async {
+            (axum::http::StatusCode::INTERNAL_SERVER_ERROR, "Unhandled internal error")
+        });
+
+    let app = axum::routing::Router::new()
+        .nest("/api", webhook_routes)
+        .route("/health", axum::routing::get(check_health))
+        .fallback_service(frontend_service);
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+
+    println!("Listening on http://127.0.0.1:3000/");
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn check_health() -> String {
+    "ONLINE".to_string()
+}
+
+async fn handle_webhook(axum::extract::Json(payload): axum::extract::Json<serde_json::Value>) {
+    println!("Received webhook: {:#?}", payload);
 }
