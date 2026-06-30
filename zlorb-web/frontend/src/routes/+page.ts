90cffa973b2870e8b1b71a6ad7aa4930dcf7edd1
Triston Armstrong <tristonarmstrong@example.com>
2026-06-29 22:23:14 -0600

    chore: delete a bunch of files
    moving into a different direction with the web ui

--- zlorb-web/frontend/src/routes/+page.ts
+++
@@ -1,6 +0,0 @@
-import type { PageLoadEvent } from './$types';
-
-export async function load({ params, fetch }: PageLoadEvent) {
-    const res = await fetch('/api/repositories');
-    return { repositories: await res.json() };
-}
