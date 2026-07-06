5a871fa37a4d3a5099e557fdc7ae005970dfe18d
Ubuntu User <ubuntu@ubuntu.local>
2026-07-06 07:29:58 +0000

    feat: update web frontend with repository list and remove unused utils

--- zlorb-web/src/lib.rs
+++ zlorb-web/src/lib.rs
@@ -1,41 +1,21 @@
 mod template;
 
 use askama::Template;
-use axum::{Router, response::Html, routing::get};
-use std::sync::LazyLock;
-use std::time::{SystemTime, UNIX_EPOCH};
+use axum::{
+    Router,
+    response::{Html, IntoResponse},
+    routing::get,
+};
+use zlorb_lib::create_config_from_toml;
+
+use crate::template::Index;
 
 const IP: &str = "0.0.0.0";
 const PORT: &str = "3000";
-const UPTIME_POLLING_INTERVAL_SEC: u64 = 30;
-
-#[derive(Template)]
-#[template(path = "index.html")]
-struct Index {
-    title: String,
-}
-
-impl Index {
-    fn new() -> Self {
-        Self {
-            title: "HTMX + Askama".to_string(),
-        }
-    }
-}
-
-static START_TIME_SECS: LazyLock<u64> = LazyLock::new(|| {
-    SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_secs() as u64
-});
-
-async fn system_uptime() -> impl IntoResponse {
-    axum::Json(std::time::SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_secs())
-}
 
 #[tokio::main]
 pub async fn run() {
-    let app = Router::new()
-        .route("/", get(get_root))
-        .route("/api/system", get(system_uptime));
+    let app = Router::new().route("/", get(get_root));
 
     let binding = format!("{IP}:{PORT}");
     let listener = tokio::net::TcpListener::bind(&binding).await.unwrap();
@@ -44,6 +24,18 @@ pub async fn run() {
     axum::serve(listener, app).await.unwrap();
 }
 
-fn get_root() -> Html<Index> {
-    Index::new().render().unwrap().into()
+async fn get_root() -> impl IntoResponse {
+    let mut index = Index::new();
+    let config = create_config_from_toml(false).unwrap().0;
+    let repos = config.repositories;
+    let mapped_repos: Vec<String> = repos.iter().map(|x| x.path.clone()).collect();
+
+    index.repos = if mapped_repos.len() < 1 {
+        None
+    } else {
+        Some(mapped_repos)
+    };
+
+    let index_render = index.render().unwrap();
+    Html(index_render)
 }
--- zlorb-web/src/template.rs
+++ zlorb-web/src/template.rs
@@ -1,15 +1,17 @@
 use askama::Template;
 
-#[derive(Template)]
+#[derive(Template, Debug)]
 #[template(path = "index.html")]
 pub(crate) struct Index {
     title: String,
+    pub repos: Option<Vec<String>>,
 }
 
 impl Index {
     pub(crate) fn new() -> Self {
         Self {
-            title: "HTMX Demo".to_string(),
+            title: "Zlorb".to_string(),
+            repos: None,
         }
     }
 }
